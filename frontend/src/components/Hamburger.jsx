54bf2c541b588c984a4cf431ca1fcc85f83dc97a
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-10-30 11:52:23 +0100

    NavBar new version

---
+++ frontend/src/components/Hamburger.jsx
@@ -0,0 +1,9 @@
+export default function Hamburger() {
+  return (
+    <button type="button" className="hamburger">
+      <div className="burger">-</div>
+      <div className="burger">-</div>
+      <div className="burger">-</div>
+    </button>
+  );
+}
