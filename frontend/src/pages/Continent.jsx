7cbc2f7b9210123532af1c39d961bb4bb971c9f6
mahdi <mahdi.mcheik@hotmail.fr>
2023-10-31 17:23:22 +0100

    adding card per dish, and continent

--- frontend/src/pages/Continent.jsx
+++ frontend/src/pages/Continent.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchByContinent, continents } from "./helpers";
+import CardDish from "../components/CardDish";
 
 function Continent() {
+  const [dishesOfContinent, setDishesOfContinent] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { continent } = useParams();
-  return <div>PAGE CONTINENT {continent}</div>;
+
+  useEffect(() => {
+    async function getData() {
+      setIsLoading(() => true);
+      await fetchByContinent(setDishesOfContinent, continents.America);
+      setIsLoading(() => false);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h3>continent loaded is {continent}</h3>
+      {!isLoading &&
+        dishesOfContinent.map((element) => <CardDish dish={element} />)}
+    </div>
+  );
 }
 
 export default Continent;
