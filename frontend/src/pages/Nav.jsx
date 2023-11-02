bed77b3e8c97483e5d7d93ac00190851f28e8f5f
Laurore83 <laurore83@yahoo.fr>
2023-11-02 09:53:26 +0100

    logoLinkHomePage

--- frontend/src/pages/Nav.jsx
+++ frontend/src/pages/Nav.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div className="container navigation">
-      <img className="logo" src="./src/assets/logo.svg" alt="logo Food Treck" />
-
+      <Link to="/">
+        <img
+          className="logo"
+          src="./src/assets/logo.svg"
+          alt="logo Food Treck"
+        />
+      </Link>
       <input
         className="searchBar"
         type="text"
